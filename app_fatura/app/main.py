098ae88787f83e9282e059529eca7a2869c16781
Magda712 <magdalena.n.mihaylova@gmail.com>
2026-02-20 10:16:58 +0100

    Update the main - Added the register route

--- app_fatura/app/main.py
+++ app_fatura/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from routers.login import router as login_router
+from routers.register import router as register_router
+
 # Create an instance of FastAPI.
 app = FastAPI()
 
 
 # Routes.
 app.include_router(login_router)
+app.include_router(register_router)
 
 
 # Test if the serve works.
