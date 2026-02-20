c41afdbdea971e99ee328044af9cc61b936fe82a
Magda712 <magdalena.n.mihaylova@gmail.com>
2026-02-20 12:28:29 +0100

    Update the register - Hash passwords added

--- app_fatura/app/routers/register.py
+++ app_fatura/app/routers/register.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from passlib.context import CryptContext
+
 
 # Create a router for the register routes.
 router = APIRouter()
 
+# Password hashing context.
+password_context = CryptContext(schemes=["bcrypt"])
+
 # Temporary database for testing.
-users_dic = {"Mimi": 123}
+users_dic = {}
+
 
 # Model fot JSON body of the post request.
 class RequestRegister(BaseModel):
     username:str
-    password:str
+    password:str = Field(max_length=72, description="The password need to be 72 charcters or less.")
 
 # Post - register route.
 # Receive JSON with the username and password.
@@ -20,6 +26,8 @@ def register(request_register: RequestRegister):
     if request_register.username in users_dic:
         raise HTTPException(status_code=409, detail= "Username already exists")
 
+    # Hash password before storing.
+    hashed_password = password_context.hash(request_register.password[:72])
     # If username doesn't exist, add it to the dictionary.
-    users_dic[request_register.username] = request_register.password
+    users_dic[request_register.username] = hashed_password
     return {"status": "Successfully added!"}
